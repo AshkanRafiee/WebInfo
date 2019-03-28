1d3f6865e715cf7c8afe62e57501b43cb98082d9
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2019-03-28 13:43:25 +0430

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,14 @@
+import builtwith
+import sys
+
+
+try:
+    host = sys.argv[1]
+    read = builtwith.parse(host)
+    data = read.items()
+
+    for a,b in data:
+        print (a+":",':'.join(b))
+except:
+    print("check syntax or your internet connection !")
+    print("*Syntax",sys.argv[0],"http://www.example.com")
